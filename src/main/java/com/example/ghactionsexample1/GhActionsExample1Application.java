bedb5ea86dac4c9dffefea0d3510952a9026371b
Yohannes <yohannesteweld@gmail.com>
2024-04-10 13:43:44 -0400

    create project

---
+++ src/main/java/com/example/ghactionsexample1/GhActionsExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.ghactionsexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhActionsExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhActionsExample1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ghactionsexample1/GhActionsExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghactionsexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhActionsExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
